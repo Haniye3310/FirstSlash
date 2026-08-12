ea6f66673c97247589fbc8fd6329df6637c167eb
Haniye <esmaeilihaniye@yahoo.com>
2026-08-12 09:55:20 +0330

    Adding equip sound

--- Source/FirstSlash/Public/Items/Weapons/Weapon.h
+++ Source/FirstSlash/Public/Items/Weapons/Weapon.h
@@ -5,7 +5,7 @@
 #include "CoreMinimal.h"
 #include "Items/Item.h"
 #include "Weapon.generated.h"
-
+class USoundBase;
 /**
  * 
  */
@@ -19,4 +19,7 @@ public:
 protected:
 	virtual void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) override;
 	virtual void OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex) override;
+private:
+	UPROPERTY(EditAnywhere)
+	USoundBase* EquipSound;
 };

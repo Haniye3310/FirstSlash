86a131b29516ae96a5540b9f9293e38a3f82bd8b
Haniye <esmaeilihaniye@yahoo.com>
2026-09-18 11:47:34 +0330

    Adding new assets for treasure Adding Destructible blueprint

--- Source/FirstSlash/Public/Items/Weapons/Weapon.h
+++ Source/FirstSlash/Public/Items/Weapons/Weapon.h
@@ -27,7 +27,8 @@ protected:
 	virtual void OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex) override;
 	UFUNCTION()
 	void OnBoxOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
-
+	UFUNCTION(BlueprintImplementableEvent)
+	void CreateFields(const FVector& FieldLocation);  
 private:
 	UPROPERTY(EditAnywhere)
 	USoundBase* EquipSound;
